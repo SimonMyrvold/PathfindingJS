ecaca7b7285be7c319ca82237fe34e8f5333c20a
SimonMyrvold <simon.johan.myrvold@gmail.com>
2022-12-12 15:07:59 +0100

    circleControllPanel

--- pathfinding/src/components/circleControllPanel.jsx
+++ pathfinding/src/components/circleControllPanel.jsx
@@ -2,8 +2,27 @@ import React from "react"
 
 export default function circleControllPanel() {
     return (
-        <nav>
-            <h1 className="underline">Hej</h1>
+        <nav className="text-center">
+
+            <div className="w-1/4">
+                <input type="checkbox" classname="rounded-full"/>
+                <h2>Rektangulär</h2>
+                <input type="checkbox" classname="rounded-full" />
+                <h2>Cirkulär</h2>
+                <input type="checkbox" classname="rounded-full" />
+                <h2>Gitter</h2>
+            </div>
+
+            <div className="mt-4">
+                <h2>Antal Rum</h2>
+                <input type="range" className="appearance-none rounded-full w-4/5 mt-4" min="0" max="100"/>
+            </div>
+
+            <div className="mt-4">
+                <h2>Complexity</h2>
+                <input type="range" className="appearance-none rounded-full w-4/5 mt-4" min="0" max="100"/>
+            </div>
+
         </nav>
     )
 }
