44dd4d4a767271c29196eae9a01ea368ffa71586
SimonMyrvold <simon.johan.myrvold@gmail.com>
2022-12-13 11:34:54 +0100

    UI & Controll

--- pathfinding/src/App.jsx
+++ pathfinding/src/App.jsx
@@ -9,30 +9,30 @@ export default function App() {
             <>
             <>
         <div className="flex flex-row space-x-4">
-        <div className="box-content h-96 w-80 p-16 border-8 bg-slate-400 rounded-[30px] flex-none">
+        <div className="box-content h-full w-80 p-16 border-8 bg-slate-400 rounded-[30px] flex-none">
 
             {/* <CircleControll /> */}
-            {/* <RectangleControll /> */}
-            <GitterControll />
+            <RectangleControll />
+            {/* <GitterControll /> */}
 
 
         </div>
-        <div className="box-content h-96 w-96 p-16 border-8 bg-slate-400 rounded-[30px] flex-1"></div>
+        <div className="box-content h-100 w-96 p-16 border-8 bg-slate-400 rounded-[30px] flex-1"></div>
         </div>
 
         <div className="text-center flex justify-center items-center h-20">
         <div className="h-12 w-1/2 border-4 bg-slate-400 rounded-[20px]">
             <div className="text-left flex flex-row h-10">
-            <button className="rounded-md w-20">
-                Load
-            </button>
-            <button className=" w-20">
-                Save
-            </button>
+                <button className="rounded-md w-20">
+                    Load
+                </button>
+                <button className=" w-20">
+                    Save
+                </button>
             </div>
         </div>
         </div>
-        </>
+            </>
             </>
             
     )
