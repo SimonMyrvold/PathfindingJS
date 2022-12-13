55ba73b41ba574caddcaf17633059a3c783dffa0
SimonMyrvold <simon.johan.myrvold@gmail.com>
2022-12-13 09:30:29 +0100

    RectangleController

---
+++ pathfinding/src/components/DropDownComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function DropdownComponent() {
+    return (
+        <div className="relative w-full lg:max-w-sm">
+            <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+                <option>ReactJS Dropdown</option>
+                <option>Laravel 9 with React</option>
+                <option>React with Tailwind CSS</option>
+                <option>React With Headless UI</option>
+            </select>
+        </div>
+    );
+}
