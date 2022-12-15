5c469b7494d01e3a6daa4f944a01e722e9acb9c9
SimonMyrvold <simon.johan.myrvold@gmail.com>
2022-12-15 10:16:33 +0100

    comments and small fixes

--- pathfinding/src/Ui_components/GameTypeSelector.jsx
+++ pathfinding/src/Ui_components/GameTypeSelector.jsx
@@ -6,20 +6,26 @@ import Gitter from "./GitterControllPanel"
 
 export default function GameTypeSelector(){
     
+    //Set the default settings to a h2 tag with text
     const [settings, setSettings] = useState(
     <h2 className="mt-8">Select a game type to see the settings</h2>
     );
     
     function TypeSelect(type){
+        //Set all checkboxes to false
+        //Set the selected checkbox to true
         document.getElementById("Rectangle").checked = false;
         document.getElementById("Circle").checked = false;
         document.getElementById("Gitter").checked = false;
         document.getElementById(type).checked = true;
+        //call the function that checks the type
         CheckType(type);
     }
 
     function CheckType (type){
+        //Check the type and set the settings accordingly
         if (type == "Circle") {
+            //Set the settings to the respective component
             setSettings(<Circle />);
         }
         
@@ -33,20 +39,21 @@ export default function GameTypeSelector(){
     }
 
     return (
-        <div>
-
+        <>
+            {/* The checkboxes for selecting the game type */}
             <div className="columns-3 text-center">
                 <h2>Rektangulär</h2>
-                <input type="checkbox" classname="rounded-full" id="Rectangle" onChange={() => TypeSelect('Rectangle')}/>
+                <input type="checkbox" className="rounded-full" id="Rectangle" onChange={() => TypeSelect('Rectangle')}/> {/*Gives the type varible the value of the checkbox*/}
                 <h2>Cirkulär</h2>
-                <input type="checkbox" classname="rounded-full" id="Circle" onChange={() => TypeSelect('Circle')}/>
+                <input type="checkbox" className="rounded-full" id="Circle" onChange={() => TypeSelect('Circle')}/> {/*Gives the type varible the value of the checkbox*/}
                 <h2>Gitter</h2>
-                <input type="checkbox" classname="rounded-full" id="Gitter" onChange={() => TypeSelect('Gitter')}/>
+                <input type="checkbox" className="rounded-full" id="Gitter" onChange={() => TypeSelect('Gitter')}/> {/*Gives the type varible the value of the checkbox*/}
             </div>
 
+            {/* The settings for the selected game type */}
             <div>{settings}</div>
             
-        </div>
+        </>
     )
 
 }
--- pathfinding/src/Ui_components/Ui.jsx
+++ pathfinding/src/Ui_components/Ui.jsx
@@ -8,17 +8,22 @@ export default function Ui() {
 
             <div className="flex flex-row space-x-4">
                 
-                <div className="box-content h-[70vh] w-1/6 p-16 border-8 bg-slate-400 rounded-[30px] flex-none">
-
+                <div className="h-[90vh] w-[60vh] p-16 border-8 bg-slate-400 rounded-[30px] flex-none">
+                    
+                    {/* The game type selector */}
                     <GameTypeSelector />
 
                 </div>
 
-                <div className="box-content w-5/6 p-16 border-8 bg-slate-400 rounded-[30px] flex-1"></div>
+                <div className="p-16 w-[200vh] border-8 bg-slate-400 rounded-[30px]">
+                    
+                    <canvas></canvas>
+
+                </div>
 
             </div>
 
-            <div className="text-center flex justify-center items-center h-16">
+            <div className="text-center flex justify-center items-center h-[10vh]">
                 <div className="h-12 w-1/2 border-4 bg-slate-400 rounded-[20px]">
                     <div className="text-left flex flex-row h-10">
                         <button className="rounded-md w-20">
